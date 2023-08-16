c0032cd237e5a1b5f56b39870b0f8f890762e27d
Vinicius Pinheiro <vinicius.pinheiro@dxc.com>
2023-08-16 20:02:25 -0300

    feat: add 'get more details' to doctors and patients

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -54,4 +54,10 @@ public class MedicoController {
     return ResponseEntity.noContent().build();
   }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> detalhar(@PathVariable Long id) {
+    Medico medico = repository.getReferenceById(id);
+
+    return ResponseEntity.ok(new DadosDetalhamentoMedico((medico)));
+  }
 }
